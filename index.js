b15bb163639c0c97a1abe6b856db634937344c06
Baskakovs <baskakovs@me.com>
2022-07-20 11:44:55 +0300

    commit tree 06392468c9ced9b5cf06572ae1300c1a6539cec4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const body = document.querySelector('body')
+const errorMessage = 'An error happened'
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName,
+        email: userEmail
+    }
+    const configObject = {
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body: JSON.stringify(formData),
+    }
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function (object){
+        body.append(object.id)
+    })
+    .catch(function (error){
+        alert('Hello')
+        body.append(error.message)
+    })
+}
